c02b36e877d37b850ec1ef258a9aa0a044954358
PhiHung160420 <67174257+PhiHung160420@users.noreply.github.com>
2020-12-06 16:14:41 +0700

    Create page home

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'App\Http\Controllers\HomeController@home');
+
+Route::get('/Trang-chu', 'App\Http\Controllers\HomeController@home');
+
